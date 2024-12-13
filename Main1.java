66cdaaf99e5dff8726ccb681c280bc5a72ee84fd
yamini225a4 <147132691+yamini225a4@users.noreply.github.com>
2024-12-13 11:18:53 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,11 @@
+public class Main1
+{
+	public static void main(String[] args) {
+	    for(int i=0;i<20;i++){
+	        if(i%2==1){
+	            System.out.println(i);
+	        }
+	    }
+	
+	}
+}
